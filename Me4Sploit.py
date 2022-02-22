74054b1fbf5bbf48d9fa59c00dde126e37a0d11f
VerTeX <62620697+pashokkok@users.noreply.github.com>
2022-02-22 21:51:30 +0300

    Add files via upload

---
+++ Me4Sploit.py
@@ -0,0 +1,67 @@
+from colorama import init 
+from colorama import Fore, Back, Style
+import time
+import smtplib
+import subprocess
+import multiprocessing
+import os
+from os import system
+
+#design me4
+print( Fore.GREEN)
+print ('=================================================')
+print ('               create by Kat̵̙̞͔͛̒͝an/\                  ')
+print ('=================================================')
+print ('               $$$$$$$$$$$$$$$$$$$$              ')
+print ('\n                                               ')
+print ('       _,.                   ')
+print ('     ,` -.)                  ')
+print ('    ( _/-\\-._               ')
+print ('   /,|`--._,-^|            , ')
+print ('   \_| |`-._/||          , | ')
+print ('     |  `-, / |         /  / ')
+print ('     |     || |        /  /  ')
+print ('      `r-._||/   __   /  /   ')
+print ('  __,-<_     )`-/  `./  /    ')
+print ('  \   `---    \   / /  /     ')
+print ('     |           |./  /      ')
+print ('     /           //  /       ')
+print (' \_/  \         |/  /        ')
+print ('  |    |   _,^- /  /         ')
+print ('  |    , ``  (\/  /_         ')
+print ('   \,.->._    \X-=/^         ')
+print ('   (  /   `-._//^`           ')
+print ('    `Y-.____(__}             ')
+print ('     |     {__)              ' )
+print ('           ()   V.1.0        ')
+
+print('[1] ActiveKey')
+print('[2] Exit')
+print('\n')
+option = input('Me4Sploit ==>  ')
+print('                                 \n \n            Copy and Past \n \n' + Fore.YELLOW + '2jsjejHEkdjutt54lkvxa83etrbKB4hUeJLiwlfu56rhcksnrix')
+key = input ("Syst-Key: ")
+
+if key == "2jsjejHEkdjutt54lkvxa83etrbKB4hUeJLiwlfu56rhcksnrix":
+	print( Fore.YELLOW )
+	print('Active')
+	time.sleep(2)
+	lists = input ('[1] Gemail-hack \n[2] B0mb3r \n[3] Mail-Spammer \nMe4Sploit ==>  ')
+	if lists == "1":
+		os.system('git clone https://github.com/Ha3MrX/Gemail-Hack')
+		
+	if lists == "2":
+		os.system('bash install_B0mb3r.sh')
+		
+	if lists == "3":
+		os.system('bash install_Mail_Spammer')
+		
+	else:
+		print('ERROR: 102' + Fore.YELLOW + '$' + Fore.RED + 'Invalid Tool')
+
+else:
+	print( Fore.RED )
+	print('ERROR: 101' + Fore.YELLOW + '$' + Fore.RED + 'Invalid Syst-Key')
+
+if option == 2 :
+	exit()
