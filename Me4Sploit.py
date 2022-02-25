9242ae46895c8e6ad5d11b1563074cf5e0206c4f
VerTeX <62620697+pashokkok@users.noreply.github.com>
2022-02-25 22:02:46 +0300

    Add files via upload

--- Me4Sploit.py
+++ Me4Sploit.py
@@ -20,8 +20,9 @@ import smtplib
 import multiprocessing
 from os import system
 import re
-version = '1.2'
+version = '1.3'
 #design me4
+
 def banner():
 	print( Fore.GREEN)
 	print ('======================================================')
@@ -30,10 +31,53 @@ def banner():
 	print('▒█▀▄▀█ █▀▀ ░█▀█░ ▒█▀▀▀█ █▀▀█ █░░ █▀▀█ ░▀░ ▀▀█▀▀ ')
 	print('▒█▒█▒█ █▀▀ █▄▄█▄ ░▀▀▀▄▄ █░░█ █░░ █░░█ ▀█▀ ░░█░░ ')
 	print('▒█░░▒█ ▀▀▀ ░░░█░ ▒█▄▄▄█ █▀▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀ ░░▀░░')
-	print('                                                                                                \       1.2      /')
-	print('                                                                                                  \________/')
+	print('                                          DarkSoftware                              \       1.3      /')
+	print('                               https://t.me/DarkSoftvare                      \________/')
+def ver_check():
+	time.sleep(3)
+	print(G + '[+]' + C + ' Проверка обновлений.....', end='')
+	ver_url = 'https://raw.githubusercontent.com/pashokkok/Me4Sploit/main/version.txt'
+	try:
+		ver_rqst = requests.get(ver_url)
+		ver_sc = ver_rqst.status_code
+		if ver_sc == 200:
+			github_ver = ver_rqst.text
+			github_ver = github_ver.strip()
+
+			if version == github_ver:
+				print(C + '[' + G + ' Актуально ' + C +']' + '\n')
+			else:
+				print(C + '[' + G + ' Доступно : {} '.format(github_ver) + C + ']' + '\n')
+				print(R + 'Пожалуйста, обновите Me4Sploit до актуальной версии')
+				print(Fore.MAGENTA)
+				newver = input('[1] Как мне обновить Me4Sploit до новой версии? \n[2] Выйти' + G + '\nMe4Sploit ==> ')
+				if newver == "1" :
+					print(Fore.MAGENTA)
+					os.system('clear')
+					print('Пункт 1: Зайдите в каталог с папкой Me4Sploit')
+					time.sleep(3)
+					print('Пункт 2: Удалите папку Me4Sploit')
+					time.sleep(3)
+					print('Пункт 3: Скачайте новый Me4Sploit командой: "git clone https://github.com/pashokkok/Me4sploit"')
+					time.sleep(3)
+					print('Пункт 4: Зайдите в папку Me4Sploit командой: "cd Me4Sploit"')
+					time.sleep(3)
+					print('Пункт 5: Запустите инсталлер командой: "bash install.sh", последующие запуски проводятся командами: "python Me4Sploit.py"')
+					time.sleep(3)
+					print('Удачи! Мы вас ждем с новой версией!')
+					time.sleep(5)
+					exit()
+				if newver == "2":
+					exit()
+				
+		else:
+			print(C + '[' + R + ' Статус : {} '.format(ver_sc) + C + ']' + '\n')
+	except Exception as e:
+		print('\n' + R + '[-]' + C + ' Исключение : ' + W + str(e))
+
+def choice():
 	print('[1] ActiveKey')
-	print('[2] Check Updates')
+	print('[2] Cracker')
 	print('[3] Exit')
 	print('\n')
 def code():
@@ -47,7 +91,7 @@ def code():
 			print( Fore.YELLOW )
 			print('Active')
 			time.sleep(2)
-			lists = input ('[1] Gemail-hack \n[2] B0mb3r \n[3] Mail-Spammer \n[4] Brute_Force \n[5] Grab-Cam \n[6] Shark \nMe4Sploit ==>  ')
+			lists = input ('[Exit][1] Gemail-hack \n[2] B0mb3r \n[3] Mail-Spammer \n[4] Brute_Force \n[5] Grab-Cam \n[6] Shark \n[7] CrossFire Ddos \nMe4Sploit ==>  ')
 			if lists == "1":
 				os.system('git clone https://github.com/Ha3MrX/Gemail-Hack')
 				
@@ -66,38 +110,44 @@ def code():
 			if lists == "6":
 				os.system('git clone https://github.com/Bhaviktutorials/shark')
 				
+			if lists == "7":
+				os.system('git clone https://github.com/crossfireTeam/crossfireMSDOS')
+				os.system('cd crossfireMSDOS')
+				os.system('cmake CMakeLists.txt')
+				os.system('chmod +x Makefile')
+				
+			if lists == "8":
+				os.system('git clone https://github.com/pashokkok/FB')
+				
 			else:
 				print('ERROR: 102' + Fore.YELLOW + '$' + Fore.RED + 'Invalid Tool')
 		
 		else:
 			print( Fore.RED )
 			print('ERROR: 101' + Fore.YELLOW + '$' + Fore.RED + 'Invalid Syst-Key')
+
 	if option == "2" :
-		print(G + '[+]' + C + ' Проверка обновлений.....', end='')
-		ver_url = 'https://raw.githubusercontent.com/pashokkok/Me4Sploit/main/version.txt'
-		try:
-			ver_rqst = requests.get(ver_url)
-			ver_sc = ver_rqst.status_code
-			if ver_sc == 200:
-				github_ver = ver_rqst.text
-				github_ver = github_ver.strip()
-	
-				if version == github_ver:
-					print(C + '[' + G + ' Актуально ' + C +']' + '\n')
-					return code()
-				else:
-					print(C + '[' + G + ' Доступно : {} '.format(github_ver) + C + ']' + '\n')
-					return code()
-			else:
-				print(C + '[' + R + ' Статус : {} '.format(ver_sc) + C + ']' + '\n')
-		except Exception as e:
-			print('\n' + R + '[-]' + C + ' Исключение : ' + W + str(e))
+		os.system('clear')
+		print('Tipo Xaker :3')
+		time.sleep(2)
+		print(Fore.GREEN)
+		print('Openning Matrix')
+		time.sleep(1)
+		print('Starting Tools')
+		time.sleep,(3)
+		print('Starting Cracker.py')
+		time.sleep(3)
+		os.system('pkg install cmatrix')
+		os.system('cmatrix')
+
 	if option == "3" :
 		exit()
-
 try:
 	banner()
+	ver_check()
+	choice()
 	code()
 	
+	
 except:
-	print("error")
+	print("end")
